791a9ea3690e37e547a78d797dd88e2a3d6c71e6
rk-db <109618734+rk-db@users.noreply.github.com>
2023-11-17 20:29:57 -0800

    Create setup.py

---
+++ Libraries/python/dabdemo/setup.py
@@ -0,0 +1,16 @@
+# Filename: setup.py
+from setuptools import setup, find_packages
+
+import dabdemo
+
+setup(
+  name = "dabdemo",
+  version = dabdemo.__version__,
+  author = dabdemo.__author__,
+  url = "https://databricks.com",
+  author_email = "rob",
+  description = "Test Pipeline",
+  packages = find_packages(include = ["dabdemo"]),
+  entry_points={"group_1": "run=dabdemo.__main__:main"},
+  install_requires = ["setuptools"]
+)
