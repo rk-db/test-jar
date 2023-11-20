9a55169e65fafc0998089057a89c96e0cdbd00c1
rk-db <109618734+rk-db@users.noreply.github.com>
2023-11-20 15:30:10 -0800

    Update deploy.py

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -98,7 +98,9 @@ try:
 
   print(os.listdir('./deploy/'))
   with ZipFile('./deploy/spark-monitoring_1.0.0.jar.zip') as zObject:
-      zObject.extractall(path='./')
+      zObject.extractall(path='./deploy/')
+  print(os.listdir('./deploy/')
+
 
   #Wait for the job to complete
   # while True:
